de6f0475f787c3c8664346deadc6779786d01329
Alex Shulgin <ash@commandprompt.com>
2013-09-02 09:09:21 -0700

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,23 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_notifications_email_matching do
+  name 'Redmine Notifications Email Matching plugin'
+  author 'Alex Shulgin <ash@commandprompt.com>'
+  description 'A plugin to match Zabbix-style PROBLEM/OK email notifications to route OK as the update to the PROBLEM ticket.'
+  version '0.1.0'
+  url 'http://github.com/commandprompt/redmine_notifications_email_matching'
+  #  author_url 'http://example.com/about'
+
+#  settings :default => {},
+#    :partial => 'settings/redmine_notifications_email_matching'
+end
+
+prepare_block = Proc.new do
+  MailHandler.send(:include, RedmineNotificationsEmailMatching::MailHandlerPatch)
+end
+
+if Rails.env.development?
+  ActionDispatch::Reloader.to_prepare { prepare_block.call }
+else
+  prepare_block.call
+end
---
+++ lib/redmine_notifications_email_matching/mail_handler_patch.rb
@@ -0,0 +1,23 @@
+module RedmineNotificationsEmailMatching
+  module MailHandlerPatch
+    unloadable
+
+    def self.included(base)
+      base.class_eval do
+        alias_method_chain :receive_issue, :notifications_email_matching
+      end
+    end
+
+    OK_SUBJECT_REGEXP = /.*OK: .*/
+
+    def receive_issue_with_notifications_email_matching
+      subject = cleaned_up_subject
+      if subject =~ OK_SUBJECT_REGEXP and
+          (issue = target_project.issues.open.order(:created_on).reverse_order.find_by_subject(subject.sub('OK', 'PROBLEM')))
+        receive_issue_reply(issue)
+      else
+        receive_issue_without_notifications_email_matching
+      end
+    end
+  end
+end
